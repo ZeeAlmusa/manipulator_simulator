fd3ef02c2c3e50378f7f2fc333dc4f069a96045b
Abdulaziz <almusa.iris@gmail.com>
2021-01-04 11:15:11 +0300

    added cylinder visualization

--- transform_y.m
+++ transform_y.m
@@ -8,7 +8,7 @@ function [gy] = transform_y(angle, translation)
 
 q = deg2rad(angle); 
 
-gu = [cos(q)  0      -sin(q) translation(1);
+gy = [cos(q)  0      -sin(q) translation(1);
       0       1      0       translation(2);
       sin(q)  0      cos(q)  translation(3);
       0       0      0      1];
